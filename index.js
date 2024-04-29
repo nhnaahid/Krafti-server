b6fb6db27f4f38377c578d6b0173215221707363
nhnaahid <nhnahid.iu@gmail.com>
2024-04-29 15:24:03 +0600

    post added

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const cors = require('cors');
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 require('dotenv').config();
 const app = express();
 const port = process.env.PORT || 5000;
@@ -33,6 +33,12 @@ async function run() {
         app.get('/crafts', async (req, res) => {
             const cursor = usersCollection.find();
             const result = await cursor.toArray();
+            res.send(result);
+        })
+        app.get('/crafts/:id', async (req, res) => {
+            const id = req.params.id;
+            const query = { _id: new ObjectId(id) };
+            const result = await usersCollection.findOne(query);
             res.send(result);
         })
         app.post('/crafts', async (req, res) => {
